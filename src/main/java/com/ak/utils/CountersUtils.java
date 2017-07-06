a760854004ed3a048c8d195f29283889a70b5a20
Aitor Molina <aitor@giak.es>
2017-07-06 11:25:06 +0200

    Counter improvement

--- src/main/java/com/ak/utils/CountersUtils.java
+++ src/main/java/com/ak/utils/CountersUtils.java
@@ -1,25 +1,20 @@
 package com.ak.utils;
 
 import java.math.BigDecimal;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.Random;
 
 /**
  * Created by amolina on 29/05/17.
  */
 public class CountersUtils {
-
-    private static final Random generator = new Random();
-    private static LocalDate date = LocalDate.now();
-    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
-
-    public static int randomGenerator() {
-        int result = generator.nextInt(99) + 1;
-        return result;
-    }
-
     public static BigDecimal randomIdGenerator(){
-        return new BigDecimal(date.format(formatter)+String.valueOf(randomGenerator()));
+    	DateFormat df = new SimpleDateFormat("yyMMddHHMM"); // Just the year, with 2 digits
+    	String formattedDate = df.format(Calendar.getInstance().getTime());
+    	return new BigDecimal(formattedDate);
     }
 }
